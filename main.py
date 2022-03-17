131f5ea15495ca96598d3c06e9e3bf3ad410eddd
Briantonio <antoniobrijmohan@gmail.com>
2022-03-17 17:31:52 -0400

    got file reader down

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
-#testing ground
-print("Hello World")
-print("Please Work")
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+
+#extract the data
+dataset = pd.read_csv('churn_Modelling.csv')
+x = dataset.iloc[:,3 :-1].values
+y = dataset.iloc[:,-1]
+
+
+print("done")
+
